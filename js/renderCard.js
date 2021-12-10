f4c52e7f91e983f595f1f3ff593d02f527bb3b08
Slawa <slawapocht4@yandex.ru>
2021-12-10 20:23:56 +0400

    day four: added search & video-play

--- js/renderCard.js
+++ js/renderCard.js
@@ -1,18 +1,32 @@
+import { getVideo } from './services.js'
+
 const listCard = document.querySelector('.other-films__list')
 const imgPath = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'
 
 const renderCard = async (data) => {
     listCard.textContent = ''
 
-    const cards = data.map(item => {
+    Promise.all(data.map(async (item) => {
     	const { vote_average, poster_path, title, name } = item
+		const video = await getVideo(item.id, item.media_type)
+		const key = video.results[0]?.key
 
         const card = document.createElement('li')
         card.classList.add('other-films__item')
         card.innerHTML = `
-			<a class="other-films__link" data-rating="${vote_average}">
+			<a class="other-films__link" 
+				data-rating="${vote_average}"
+				${key ? 
+					`href="https://youtu.be/${key}"`: 
+					''
+				}
+			>
 	            <img class="other-films__img" 
-	            	 src="${imgPath}${poster_path}"
+	            	 src="
+	            	 ${poster_path ?
+	            	 `${imgPath}${poster_path}` :
+	            	 './img/poster_none.png'
+	            	}"
 	            	 alt="постер ${title || name}"
 	            >
 	        </a>
@@ -23,10 +37,9 @@ const renderCard = async (data) => {
 			link.removeAttribute('data-rating')
 		}
 
-        listCard.append(card)
+        
         return card
-    })
-
+    })).then(cards => listCard.append(...cards))
 }
 
 export default renderCard
--- js/renderVideo.js
+++ js/renderVideo.js
@@ -1,35 +1,51 @@
-import { getTrends } from './services.js'
+import { getTrends, getVideo } from './services.js'
 import renderCard from './renderCard.js'
 
 
 const filmWeek = document.querySelector('.film-week')
 const IMG_PATH = 'https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/'
 
-const firstRender = data => {
-	const { backdrop_path, original_title, title, original_name, name, vote_average, video } = data
-	
+const firstRender = (data, video) => {
+	const { 
+			backdrop_path: backdropPath, 
+			original_title: originalTitle, 
+			title, 
+			original_name: originalName, 
+			name, 
+			vote_average: voteAverage
+			} = data
+	// const { key } = video
+	// console.log(keyVideo)
 	filmWeek.innerHTML = `
 		<div class="container film-week__container" 
-			 data-rating="${vote_average}">
+			 data-rating="${voteAverage}">
             <div class="film-week__poster-wrapper">
                 <img class="film-week__poster" 
-                	 src="${IMG_PATH}${backdrop_path}"
+                	 src="${IMG_PATH}${backdropPath}"
                 	 alt="постер ${title || name}">
                 <p class="film-week__title_origin">
-                	${original_title || original_name}
+                	${originalTitle || originalName}
                 </p>
             </div>
             <h2 class="film-week__title">${title || name}</h2>
-            ${video ? '<a class="film-week__watch-trailer tube" href="https://youtu.be/V0hagz_8L3M" aria-label="смотреть трейлер"></a>' : ''}
+            ${video ?
+            	`<a class="film-week__watch-trailer tube" 
+            		href="https://youtu.be/${video.key}" 
+            		aria-label="смотреть трейлер"></a>` :
+            	''}
         </div>
 	`
 }
 
 const renderVideo = async () => {
 	const data = await getTrends()
+
 	const [ firstCard, ...otherCard ] = data.results
 	otherCard.length = 12
-	firstRender(firstCard)
+
+	const video = await getVideo(firstCard.id, firstCard.media_type)
+	
+	firstRender(firstCard, video.results[0])
 	renderCard(otherCard)
 }
 
